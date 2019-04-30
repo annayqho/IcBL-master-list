83f85fec897e1e9931bc25e5294c8403fa64ba09
annayqho <annayqho@gmail.com>
2019-04-30 14:57:09 -0700

    basic layout of table

--- code/tables/print_sample_table.py
+++ code/tables/print_sample_table.py
@@ -2,4 +2,63 @@
 Print a table of the Ic-BL sample from ZTF
 """
 
+import numpy as np
+from math import floor, log10
+from astropy.time import Time
+from astropy.cosmology import Planck15
 
+
+def round_sig(x, sig=2):
+    if x < 0:
+        return -round(-x, sig-int(floor(log10(-x)))-1)
+    return round(x, sig-int(floor(log10(x)))-1)
+
+
+def ndec(num):
+    dec = str(num).split('.')[-1]
+    return len(dec)
+
+
+headings = np.array(
+        ['ZTF ID', 'RA (hms)', 'Dec (dms)', 'Disc (JD)', 
+         '$z$', 'Ref.'])
+label = "sample"
+caption = "Sample of Ic-BL SNe from ZTF in the period May 2018 - May 2019"
+
+# Print the table headers
+ncol = len(headings)
+colstr = ""
+colstr += 'l'
+for col in np.arange(ncol-1): colstr+="r"
+print(colstr)
+
+colheadstr = ""
+for col in np.arange(ncol-1):
+    colheadstr += "\colhead{%s} & " %headings[col]
+colheadstr += "\colhead{%s}" %headings[-1]
+
+rowstr = ""
+for col in np.arange(ncol-1):
+    rowstr += "%s & "
+rowstr += "%s \\\ \n"
+
+outputf = open("table_%s.txt" %label, "w")
+outputf.write("\\startlongtable \n")
+outputf.write("\\begin{deluxetable}{%s} \n" %colstr)
+outputf.write("\\tablecaption{%s\label{tab:%s}} \n" %(caption,label))
+outputf.write("\\tablewidth{0pt} \n")
+outputf.write("\\tablehead{ %s } \n" %colheadstr)
+#outputf.write("\\rotate \n")
+outputf.write("\\tabletypesize{\scriptsize} \n")
+outputf.write("\startdata \n")
+
+datadir = "/Users/annaho/Dropbox/Projects/Research/IcBL/data"
+names = np.loadtxt(datadir + "/ztf.dat")
+nobj = len(names)
+
+for ii in np.arange(nobj):
+    row = rowstr %(names[ii])
+    outputf.write(row)
+
+outputf.write("\enddata \n")
+outputf.write("\end{deluxetable} \n")
