afe5ae37de0f3a7c045ef63c3289094bd6f42b71
annayqho <annayqho@gmail.com>
2019-05-06 15:50:56 -0700

    abs mag, put names, labels

--- code/paper_plots/plot_lc.py
+++ code/paper_plots/plot_lc.py
@@ -16,8 +16,10 @@ zquery = query.ZTFQuery()
 
 # get the names of the Ic-BL SNe
 datadir = "/Users/annaho/Dropbox/Projects/Research/IcBL/data"
-names = np.loadtxt(datadir + "/ztf.dat", dtype=str)
+dat = np.loadtxt(datadir + "/ztf.dat", dtype=str, delimiter=',')
+names = dat[:,0]
 nobj = len(names)
+redshift = dat[:,1].astype(float)
 
 # make a grid of say, 4 across
 ncol = 4
@@ -32,6 +34,7 @@ for ii,name in enumerate(names):
     lc_dict = marshal.get_local_lightcurves(name)
 
     jd = lc_dict['jdobs'].values
+    dm = Planck15.distmod(z=redshift[ii]).value
     mag = lc_dict['mag'].values
     emag = lc_dict['emag'].values
     filt = lc_dict['filter'].values
@@ -39,21 +42,36 @@ for ii,name in enumerate(names):
     choose = np.logical_and(filt == 'r', mag < 99)
     if sum(choose) > 0:
         ax.errorbar(
-                jd[choose]-jd[choose][0], mag[choose], yerr=emag[choose], 
-                c='#140b34', fmt='s', ms=5)
+                (jd[choose]-jd[choose][0])/(1+redshift[ii]), mag[choose]-dm, 
+                yerr=emag[choose], c='#140b34', fmt='s', ms=5)
     
     choose = np.logical_and(filt == 'g', mag < 99)
     if sum(choose) > 0:
         ax.errorbar(
-                jd[choose]-jd[choose][0], mag[choose], yerr=emag[choose], 
-                c='#e55c30', fmt='o', ms=5)
+                (jd[choose]-jd[choose][0])/(1+redshift[ii]), mag[choose]-dm, 
+                yerr=emag[choose], c='#e55c30', fmt='o', ms=5)
 
-    ax.set_title(name)
+    ax.text(0.95, 0.95, name.split("18")[1], transform=ax.transAxes, 
+            fontsize=12, horizontalalignment='right',
+            verticalalignment='top')
     ax.tick_params(labelsize=12, axis='both')
+
+# Get rid of the extra panels
+for ax in axarr.reshape(-1)[nobj:]:
+    ax.set_visible(False)
+    
+fig.subplots_adjust(hspace=0, wspace=0)
+
+fig.text(0.5, 0.04,
+        "Rest-Frame Time Since Observed $r$-band Max [d]", fontsize=16,
+        ha='center')
+
+fig.text(0.04, 0.5, "Absolute Magnitude (AB)", 
+        ha='center', va='center', fontsize=16, rotation='vertical')
 
 ax.set_xlim(-20,100)
 ax.invert_yaxis()
-plt.tight_layout()
+#plt.tight_layout()
 
-#plt.show()
-plt.savefig("lc_grid.eps", format='eps', dpi=1000)
+plt.show()
+#plt.savefig("lc_grid.eps", format='eps', dpi=1000)
